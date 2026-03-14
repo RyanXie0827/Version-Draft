b065d802772962a95e66f6bafbf8bb6b50349717
GitHub Action <action@github.com>
2026-03-14 07:28:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/ryanxie0827/versiondraft/MyBundle.kt
+++ src/main/kotlin/com/github/ryanxie0827/versiondraft/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ryanxie0827.versiondraft
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ryanxie0827/versiondraft/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ryanxie0827/versiondraft/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ryanxie0827.versiondraft.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ryanxie0827.versiondraft.MyBundle
+import com.github.ryanxie0827.versiondraft.services.MyProjectService
 import javax.swing.JButton
 
 
